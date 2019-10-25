3f8d7140636b26a3d4e996d121dc35540d6e50a2
sttraifi <56055720+sttraifi@users.noreply.github.com>
2019-10-25 18:04:30 -0400

    Added gender.py

---
+++ datavengers/model/gender.py
@@ -0,0 +1,13 @@
+from predictor import Predictor
+
+
+class Gender(Predictor):
+
+    def __init__(self):
+        super().__init__()
+    
+    def train(self, raw_train_data):
+        pass
+
+    def predict(self, raw_test_data):
+        pass
