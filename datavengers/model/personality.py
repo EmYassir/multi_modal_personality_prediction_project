85b143f3ef4aab5aff006e0bbb1a4c70a6456bba
EmYassir <yassir.elmesbahi@outlook.com>
2019-10-27 19:00:47 -0400

    Personality predictions

--- datavengers/model/personality.py
+++ datavengers/model/personality.py
@@ -1,14 +1,134 @@
-from predictor import Predictor
+import numpy as np
+import pandas as pd
+import pickle as pkl
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LassoCV
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from datavengers.model.predictor import Predictor
 
 
 class Personality(Predictor):
 
-    def __init__(self):
+    def __init__(self, use_predefined_model = False):
         super().__init__()
-        # save the model
+        self._layers = tuple([50,100,50])
+        if use_predefined_model == False:
+            self._model = MLPRegressor(hidden_layer_sizes=self._layers)
+        else:
+            fd = open('./datavengers/persistence/personality/personality.model', 'rb')
+            self._model = pkl.load(fd)
     
-    def train(self, raw_train_data):
-        pass
+    def _rescale(self, x, oldmin, oldmax, newmin, newmax):
+        coeff = float((newmax-newmin)/(oldmax - oldmin))
+        return coeff * (x + oldmin) - newmin
 
+    def _process_raw_data(self, raw_data):
+        nrc = raw_data.get_nrc()
+        profiles = raw_data.get_profiles()
+        
+        # Extracting data and combining it together
+        # 1) : Ope
+        yOpedf = profiles[['userid','ope']]
+        yOpedf.columns = ['userId','ope']
+        xOpe = pd.merge(nrc, yOpedf, on='userId', how='inner')
+        
+        # 2) : Ope + Con
+        yCondf = profiles[['userid','con']]
+        yCondf.columns = ['userId','con']
+        xCon = pd.merge(xOpe, yCondf, on='userId', how='inner')
+        
+        # 3) : Ope + Con + Ext
+        yExtdf = profiles[['userid','ext']]
+        yExtdf.columns = ['userId','ext']
+        xExt = pd.merge(xCon, yExtdf, on='userId', how='inner')
+        
+        # 4) : Ope + Con + Ext + Agr
+        yAgrdf = profiles[['userid','agr']]
+        yAgrdf.columns = ['userId','agr']
+        xAgr = pd.merge(xExt, yAgrdf, on='userId', how='inner')
+        
+        # 5) : Ope + Con + Ext + Agr + Neu
+        yNeudf = profiles[['userid','neu']]
+        yNeudf.columns = ['userId','neu']
+        
+        # Merge everything and Remove Ids column
+        df = (pd.merge(xAgr, yNeudf, on='userId', how='inner')).drop (columns = 'userId')
+        return df
+    
+    def _preprocess_data(self, data_frame):
+        # Transform into numpy array
+        # Split features from targets
+        X_np = data_frame.to_numpy()
+        # rescale 'surprise' column
+        X_np[:,8] = self._rescale(X_np[:,8], 0.0, 0.5, 0.0, 1.0)
+        n_cls= 5
+        n = X_np.shape[1]
+        return X_np[:,:n - n_cls], X_np[:,n - n_cls:]
+    
+    def _transform_data(self, X, n = 2):
+        pca = PCA(n_components = n)
+        return pca.fit_transform(X)
+    
+    def _extract_features(self, X):
+        return X[:, 1:-1] # removing positiveness 
+    
+    def _rescale_features(self, X):
+        return X[:, 1:-1] # removing positiveness 
+    
+    def _process(self, raw_data):
+        df = self._process_raw_data(raw_data)
+        X, y = self._preprocess_data(df)
+        #X = self._transform_data(X)
+        X = self._extract_features(X)
+        return X, y
+    
+    # Public methods
+
+    def train(self, raw_train_data):
+        # Preprocess
+        X, y = self._process(raw_train_data)
+        # Training
+        self._model.fit(X, y)
+        
+        # Train on
+    
     def predict(self, raw_test_data):
-        pass
+        # Preprocess
+        X, y = self._process(raw_test_data)
+        return self._model.predict(X)
+    
+    def fit(self, raw_train_data):
+        # Preprocess
+        X, y = self._process(raw_train_data)
+        
+        # Split data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
+        # Train on train data
+        self._model.fit(X_train, y_train)
+        
+        # Predict on train and test sets
+        y_p = self._model.predict(X_train)
+        print('Train score:')
+        print(self.get_rmse_score(y_p, y_train))
+        y_p = self._model.predict(X_test)
+        print('Test score:')
+        print(self.get_rmse_score(y_p, y_test))
+    
+    def get_rmse_score(self, y_pred, y):
+        return np.sqrt(np.mean((y_pred - y)**2, axis = 0))
+    
+    def update_model(self, new_model):
+        self._model = new_model
+    
+    def load_model(self):
+        fd = open('./datavengers/persistence/personality/personality.model', 'r')
+        self._model = pkl.load(fd)
+    
+    def save_model(self):
+        fd = open('./datavengers/persistence/personality/personality.model', 'wb')
+        pkl.dump(self._model, fd) 
--- datavengers/model/predictor.py
+++ datavengers/model/predictor.py
@@ -1,8 +1,14 @@
-from data import Data
+
 
 class Predictor:
 
-    def __init__(self):
+    def __init__(self, useExisting = False):
+        pass
+    
+    def load_model(self):
+        pass
+    
+    def save_model(self):
         pass
     
     def train(self, raw_train_data):
