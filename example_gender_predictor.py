3c4303915823f510b8e4e4d753f3e0f6aa6d6956
sttraifi <56055720+sttraifi@users.noreply.github.com>
2019-10-27 21:49:32 -0400

    Gender with prediction based on page likes only

---
+++ example_gender_predictor.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Sabi
+
+Simple example to show how to use the predictor 
+"""
+
+from datavengers.model.data import Data
+from datavengers.model.predictor import Predictor
+from datavengers.model.gender import Gender
+
+liwc_train = './data/Train/Text/liwc.csv'
+nrc_train = './data/Train/Text/nrc.csv'
+rel_train = './data/Train/Relation/Relation.csv'
+oxf_train = './data/Train/Image/oxford.csv'
+pro_train = './data/Train/Profile/Profile.csv'
+    
+#### Test data
+liwc_test = './data/Public_Test/Text/liwc.csv'
+nrc_test = './data/Public_Test/Text/nrc.csv'
+rel_test = './data/Public_Test/Relation/Relation.csv'
+oxf_test = './data/Public_Test/Image/oxford.csv'
+pro_test = './data/Public_Test/Profile/Profile.csv'
+
+#### Creating data containers
+print('Preparing data...')
+train_data_wrapper = Data(liwc_train, nrc_train, rel_train, oxf_train, pro_train)
+test_data_wrapper = Data(liwc_test, nrc_test, rel_test, oxf_test, pro_test)
+    
+print('Instantiating model: Calling with no arguments (\'\')...')
+model = Gender()
+
+print('Using pre-trained model...')
+# use False as 2nd argument to retrain...
+model.train(train_data_wrapper)
+
+print('Generating predictions on the test set...')
+pred1 = model.predict(test_data_wrapper)
+print('-> predicted:')
+print(pred1)
