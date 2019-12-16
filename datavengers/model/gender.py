10d3b341018587285db8937d63fd1121094ff51f
sttraifi <56055720+sttraifi@users.noreply.github.com>
2019-12-16 19:46:19 +0100

    Added NRC to LIWC predictions

--- datavengers/model/gender.py
+++ datavengers/model/gender.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, QuantileTransformer
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -16,11 +16,11 @@ from keras.layers import Dense
 from datavengers.model.data import Data
 from datavengers.model.predictor import Predictor
 
-class Gender(Predictor):
+class Gender():
     def __init__(self):
       self._relation_model = None
       self._oxford_model = None
-      self._liwc_model = None
+      self._nrc_liwc_model = None
 
     def load_relation_model(self):
         with open('./datavengers/persistence/gender/relation.model', 'rb') as fd:
@@ -38,13 +38,13 @@ class Gender(Predictor):
         with open('./datavengers/persistence/gender/oxford.model', 'wb') as fd:
             pkl.dump(self._oxford_model, fd) 
 
-    def load_liwc_model(self):
-        with open('./datavengers/persistence/gender/liwc.model', 'rb') as fd:
-            self._liwc_model = pkl.load(fd)
+    def load_nrc_liwc_model(self):
+        with open('./datavengers/persistence/gender/nrc-liwc.model', 'rb') as fd:
+            self._nrc_liwc_model = pkl.load(fd)
     
-    def save_liwc_model(self):
-        with open('./datavengers/persistence/gender/liwc.model', 'wb') as fd:
-            pkl.dump(self._liwc_model, fd) 
+    def save_nrc_liwc_model(self):
+        with open('./datavengers/persistence/gender/nrc-liwc.model', 'wb') as fd:
+            pkl.dump(self._nrc_liwc_model, fd) 
 
     def format_relations(self, raw_relation_data):
       relations = raw_relation_data
@@ -57,17 +57,19 @@ class Gender(Predictor):
       if (preTrained):
         self.load_relation_model()
         self.load_oxford_model()
-        self.load_liwc_model()
+        self.load_nrc_liwc_model()
 
       else:
         # train the Model for Relation data 
         self.train_relations(raw_train_data)
 
         # train the Model for Oxford data
+        print("training oxford")
         self.train_oxford(raw_train_data)
 
-        # train the Model for LIWC data
-        self.train_liwc(raw_train_data)
+        # train the Model for LIWC/NRC data
+        print("training NRC/LIWC")
+        self.train_nrc_liwc(raw_train_data)
       return
 
     def train_relations(self, raw_train_data):
@@ -107,9 +109,9 @@ class Gender(Predictor):
       Y_oxford = oxford_unique_face['gender']
       X_oxford = oxford_unique_face.drop(['userid','face_size','faceid','gender'], axis = 1)
         
-      # We normalize the data
-      min_max_scaler = MinMaxScaler()
-      X_oxford_scaled = min_max_scaler.fit_transform(X_oxford)
+      # We normalize the data with Robust scaler
+      r_scaler = RobustScaler()
+      X_oxford_scaled = r_scaler.fit_transform(X_oxford)
 
       # We define our NN model using Keras, one hidden layer with 100 neurons, Activation layer using sigmoid
       self._oxford_model = Sequential([
@@ -118,58 +120,64 @@ class Gender(Predictor):
           ])
 
       self._oxford_model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
-      self._oxford_model.fit(X_oxford_scaled, Y_oxford, batch_size=32, epochs=30)
+      self._oxford_model.fit(X_oxford_scaled, Y_oxford, batch_size=32, epochs=35)
       self.save_oxford_model()
       return
 
-    def train_liwc(self, raw_train_data):
+    def train_nrc_liwc(self, raw_train_data):
+
       tags = raw_train_data.get_profiles()[['userid','gender']]
       tags['gender'] = tags['gender'].astype(int)
 
       liwc_train_df = raw_train_data.get_liwc().copy()
+      # liwc file uses 'userId' instead of 'userid' ... so we apply lower case
       liwc_train_df.columns = [x.lower() for x in liwc_train_df.columns]
       liwc = pd.merge(tags, liwc_train_df, on="userid")
 
-      # We prepare the data for training
-      Y_liwc = liwc['gender']
-      X_liwc = liwc.drop(['gender','userid'], axis = 1)
-        
-      # We normalize the data
-      min_max_scaler = MinMaxScaler()
-      X_liwc_scaled = min_max_scaler.fit_transform(X_liwc)
+      nrc_train_df = raw_train_data.get_nrc().copy()
+      nrc_train_df.columns = [x.lower() for x in nrc_train_df.columns]
 
-      # We define our NN model using Keras, one hidden layer with 100 neurons
-      self._liwc_model = Sequential([
-          Dense(100, activation='relu', kernel_initializer='random_normal', input_dim=X_liwc_scaled.shape[1]),
+      # We merge NRC and LIWC dataframes
+      nrc_liwc = pd.merge(liwc, nrc_train_df, on="userid")
+
+      # We prepare the data for training
+      Y_nrc_liwc = nrc_liwc['gender']
+      X_nrc_liwc = nrc_liwc.drop(['gender','userid'], axis = 1)
+        
+      # We normalize the data using QuantileTransformer
+      q_scaler = QuantileTransformer(100)
+      X_nrc_liwc_scaled = q_scaler.fit_transform(X_nrc_liwc)
+
+      # We define our NN model using Keras, one hidden layer with 100 neurons, Activation layer using sigmoid
+      self._nrc_liwc_model = Sequential([
+          Dense(100, activation='relu', kernel_initializer='random_normal', input_dim=X_nrc_liwc_scaled.shape[1]),
           Dense(1, activation='sigmoid'),
           ])
 
-      self._liwc_model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
-      self._liwc_model.fit(X_liwc_scaled, Y_liwc, batch_size=32, epochs=30)
-      self.save_liwc_model()
+      self._nrc_liwc_model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+      self._nrc_liwc_model.fit(X_nrc_liwc_scaled, Y_nrc_liwc, batch_size=32, epochs=15)
+      self.save_nrc_liwc_model()
       return
-
 
     def predict(self, raw_test_data):
       oxford_pred_df = self.predict_on_oxford(raw_test_data)
-      liwc_pred_df = self.predict_on_liwc(raw_test_data)
+      nrliwc_pred_df = self.predict_on_nrc_liwc(raw_test_data)
 
       # We replace 'liwc predictions' by 'oxford predictions', whenever there is an oxford prediction
       # to do this we join the 2 dataframes
-      liwc_pred_df = liwc_pred_df.set_index(['userid'])
+      nrliwc_pred_df = nrliwc_pred_df.set_index(['userid'])
       oxford_pred_df = oxford_pred_df.set_index(['userid'])
 
-      liwc_pref_df_copy = liwc_pred_df.copy()
-      liwc_pred_df['gender'] = oxford_pred_df['gender']
+      nrliwc_pref_df_copy = nrliwc_pred_df.copy()
+      nrliwc_pred_df['gender'] = oxford_pred_df['gender']
       
-      # the dataframe below contains the merge of oxford predictions that replace liwc predictions 
-      merged_df = liwc_pred_df.combine_first(liwc_pref_df_copy)
+      merged_df = nrliwc_pred_df.combine_first(nrliwc_pref_df_copy)
       
+      print ("total prediction size = ", merged_df.shape)
       return np.array(merged_df['gender']).astype(int)
 
-
-    def predict_on_relation(self, raw_test_data):
-      # Not used in the submission of Dec 2nd, as we are testing LIWC this time
+    
+    def predict_on_relation(self, raw_test_data):  
       merged_relations = self.format_relations(raw_test_data.get_relation())
       X_test = merged_relations['like_id']
       prediction = self._relation_model.predict(X_test)
@@ -196,11 +204,12 @@ class Gender(Predictor):
       X_test = oxford_data.drop(['userid','face_size','faceid'], axis = 1)
 
       # We normalize the data
-      min_max_scaler = MinMaxScaler()
-      X_scaled = min_max_scaler.fit_transform(X_test)
+      r_scaler = RobustScaler()
+      X_scaled = r_scaler.fit_transform(X_test)
 
-      # We use Keras predict_classes to get the class label
       prediction = self._oxford_model.predict_classes(X_scaled)
+
+      print("oxford prediction: ", len(prediction))
 
       df = pd.DataFrame()
       df['userid'] = oxford_data['userid']
@@ -208,23 +217,28 @@ class Gender(Predictor):
       return df
 
 
-    def predict_on_liwc(self, raw_test_data):
+    def predict_on_nrc_liwc(self, raw_test_data):
       liwc_test_df = raw_test_data.get_liwc().copy()
+      nrc_test_df = raw_test_data.get_nrc().copy()
       liwc_test_df.columns = [x.lower() for x in liwc_test_df.columns]
+      nrc_test_df.columns = [x.lower() for x in nrc_test_df.columns]
       test_users = raw_test_data.get_profiles()['userid']
       liwc_data = pd.merge(test_users, liwc_test_df, on="userid")
 
-      # We remove non-required fields
-      X_test = liwc_data.drop(['userid'], axis = 1)
+      # We make of fusion of NRC and LIWC data
+      nrc_liwc_data = pd.merge(liwc_data, nrc_test_df, on="userid")
 
-      # We normalize the data
-      min_max_scaler = MinMaxScaler()
-      X_scaled = min_max_scaler.fit_transform(X_test)
+      X_test = nrc_liwc_data.drop(['userid'], axis = 1)
 
-      # We use Keras predict_classes to get the class label
-      prediction = self._liwc_model.predict_classes(X_scaled)
+      # We scale the data using QuantileTransformer
+      q_scaler = QuantileTransformer(100)
+      X_scaled = q_scaler.fit_transform(X_test)
+      
+      prediction = self._nrc_liwc_model.predict_classes(X_scaled)
+
+      print("NRC LIWC prediction: ", len(prediction))
 
       df = pd.DataFrame()
-      df['userid'] = liwc_data['userid']
+      df['userid'] = nrc_liwc_data['userid']
       df['gender'] = prediction
       return df
